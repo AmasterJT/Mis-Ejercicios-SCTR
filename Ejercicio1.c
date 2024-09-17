e16198c85f4e0b7822141aa78e20e8ec60512cb7
AmasterJT <amasterjt@gmail.com>
2024-09-17 23:31:36 +0200

    clear the folder

--- Ejercicio1.c
+++ Ejercicio1.c
@@ -12,63 +12,63 @@ de incidencias por pieza.
 
 int main() {
 
-  int incidencias[] = {85, 72, 91, 72, 72, 37, 37, 91, 37, 72, 37, 85, 37, 91};
-  int numIncidencias = 14;
+    int incidencias[] = {85, 72, 91, 72, 72, 37, 37, 91, 37, 72, 37, 85, 37, 91};
+    int numIncidencias = 14;
 
-  // int longitud = sizeof(incidencias) / sizeof(incidencias[0]);
-  // printf("%d", longitud);
+    // int longitud = sizeof(incidencias) / sizeof(incidencias[0]);
+    // printf("%d", longitud);
 
-  int ExisteMaquina = 0;
-  int posicionador = 0;
-  int ContadorIncidencias = 0;
+    int ExisteMaquina = 0;
+    int posicionador = 0;
+    int ContadorIncidencias = 0;
 
-  int maquinas[10] = {
-      0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  };
-  int incidenciaMaquina[10] = {
-      0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  };
+    int maquinas[10] = {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    };
+    int incidenciaMaquina[10] = {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    };
 
-  // hallamos todas las maquinas que tienen alguna incidencia
+    // hallamos todas las maquinas que tienen alguna incidencia
 
-  for (int i = 0; i < numIncidencias; i++) {
-    int incidencia = incidencias[i];
-    ExisteMaquina = 0;
+    for (int i = 0; i < numIncidencias; i++) {
+        int incidencia = incidencias[i];
+        ExisteMaquina = 0;
 
-    for (int j = 0; j < 10; j++) // vemos si la incidencia ya esta registrada en
-                                 // el registro de maquinas
-    {
-      int maquina = maquinas[j];
+        for (int j = 0; j < 10; j++) // vemos si la incidencia ya esta registrada en
+                                     // el registro de maquinas
+        {
+            int maquina = maquinas[j];
 
-      if (incidencia == maquina) {
-        ExisteMaquina++; // la maquina ya esta registrada
-      }
+            if (incidencia == maquina) {
+                ExisteMaquina++; // la maquina ya esta registrada
+            }
+        }
+
+        if (ExisteMaquina == 0) // la maquina no esta registrada
+        {
+            maquinas[posicionador] = incidencia; // registramos la maquina
+            posicionador++;                      // cambiamos la posicion para guardar una maquina nueva
+        }
     }
 
-    if (ExisteMaquina == 0) // la maquina no esta registrada
-    {
-      maquinas[posicionador] = incidencia; // registramos la maquina
-      posicionador++; // cambiamos la posicion para guardar una maquina nueva
-    }
-  }
+    // procedemos a contar la cantidad de incidencias que se produjeron en cada
+    // maquina
 
-  // procedemos a contar la cantidad de incidencias que se produjeron en cada
-  // maquina
+    for (int i = 0; i < 10; i++) {
+        ContadorIncidencias = 0;
+        int maquina = maquinas[i];
 
-  for (int i = 0; i < 10; i++) {
-    ContadorIncidencias = 0;
-    int maquina = maquinas[i];
+        for (int j = 0; j < numIncidencias; j++) {
+            if (maquina == incidencias[j]) {
+                ContadorIncidencias++;
+            }
+        }
 
-    for (int j = 0; j < numIncidencias; j++) {
-      if (maquina == incidencias[j]) {
-        ContadorIncidencias++;
-      }
+        if (maquina != 0) {
+            incidenciaMaquina[i] = ContadorIncidencias;
+        }
     }
 
-    if (maquina != 0) {
-      incidenciaMaquina[i] = ContadorIncidencias;
-    }
-  }
-
-  return 0;
+    return 0;
 }
